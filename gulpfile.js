06fabf9cd20158d64769fd6e0131633cc97c9566
Vincent Giersch <vincent@giersch.fr>
2022-08-16 17:45:58 +0200

    update(redoc): update to 2.0.0-rc.75

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,8 @@ const swaggerRepo = require('swagger-repo');
 
 const DIST_DIR = 'web_deploy';
 
-
 gulp.task('edit', function() {
-  portfinder.getPort({ port: process.env.PORT_SWAGGER_EDITOR || 5000 }, function (err, port) {
+  portfinder.getPort({ port: process.env.PORT_SWAGGER_EDITOR || 5001 }, function (err, port) {
     const app = connect();
     app.use(swaggerRepo.swaggerEditorMiddleware());
     app.listen(port);
@@ -37,7 +36,7 @@ gulp.task('watch', function () {
 
 
 gulp.task('serve', gulp.parallel('build', 'watch', 'edit'), function() {
-  portfinder.getPort({ port: process.env.PORT_REDOC || 3000 }, function (err, port) {
+  portfinder.getPort({ port: process.env.PORT_REDOC || 5000 }, function (err, port) {
     gulpConnect.server({
       root: [DIST_DIR],
       livereload: true,
