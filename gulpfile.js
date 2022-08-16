f009db9311fe0d91c23d39a775de6334583dc78d
Vincent Giersch <vincent@giersch.fr>
2022-08-16 17:45:32 +0200

    chore(build): tasks for gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,19 @@
-var gulp = require('gulp');
-var util = require('gulp-util')
-var gulpConnect = require('gulp-connect');
-var connect = require('connect');
-var cors = require('cors');
-var path = require('path');
-var exec = require('child_process').exec;
-var portfinder = require('portfinder');
-var swaggerRepo = require('swagger-repo');
+const gulp = require('gulp');
+const util = require('gulp-util')
+const gulpConnect = require('gulp-connect');
+const connect = require('connect');
+const cors = require('cors');
+const path = require('path');
+const exec = require('child_process').exec;
+const portfinder = require('portfinder');
+const swaggerRepo = require('swagger-repo');
 
-var DIST_DIR = 'web_deploy';
+const DIST_DIR = 'web_deploy';
 
-gulp.task('serve', ['build', 'watch', 'edit'], function() {
-  portfinder.getPort({port: 3000}, function (err, port) {
-    gulpConnect.server({
-      root: [DIST_DIR],
-      livereload: true,
-      port: port,
-      middleware: function (gulpConnect, opt) {
-        return [
-          cors()
-        ]
-      }
-    });
-  });
-});
 
 gulp.task('edit', function() {
-  portfinder.getPort({port: 5000}, function (err, port) {
-    var app = connect();
+  portfinder.getPort({ port: process.env.PORT_SWAGGER_EDITOR || 5000 }, function (err, port) {
+    const app = connect();
     app.use(swaggerRepo.swaggerEditorMiddleware());
     app.listen(port);
     util.log(util.colors.green('swagger-editor started http://localhost:' + port));
@@ -41,10 +27,26 @@ gulp.task('build', function (cb) {
   });
 });
 
-gulp.task('reload', ['build'], function () {
+gulp.task('reload', gulp.series('build'), function () {
   gulp.src(DIST_DIR).pipe(gulpConnect.reload())
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['spec/**/*', 'web/**/*'], ['reload']);
+  gulp.watch(['spec/**/*', 'web/**/*'], gulp.series('reload'));
+});
+
+
+gulp.task('serve', gulp.parallel('build', 'watch', 'edit'), function() {
+  portfinder.getPort({ port: process.env.PORT_REDOC || 3000 }, function (err, port) {
+    gulpConnect.server({
+      root: [DIST_DIR],
+      livereload: true,
+      port: port,
+      middleware: function (gulpConnect, opt) {
+        return [
+          cors()
+        ]
+      }
+    });
+  });
 });
